7ab5b03cfc406b92608a757091b5d2b1089f1796
Gaurav <gauravmahor7061@gmail.com>
2025-06-26 19:09:38 +0530

    Build user model, controller and route, and also implemented user authentication

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
+import userRoute from "./routes/user.route.js";
 
 dotenv.config({});
 
@@ -20,6 +21,8 @@ app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3000;
 
+//all api's here
+app.use("/api/v1/user", userRoute);
 
 app.listen(PORT,()=>{
     connectDB();
