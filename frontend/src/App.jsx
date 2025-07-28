7dc49520c6ebe93f88e6d1a38435b01716afec2f
Gaurav <gauravmahor7061@gmail.com>
2025-07-28 17:58:12 +0530

    Fix some bugs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Register from "./pages/Register"
 import Login from "./pages/Login"
 import Home from "./pages/Home" // if you have one
+import ViewProfile from "./pages/ViewProfile"
 import "./App.css"
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/profile" element={<ViewProfile />} />
       </Routes>
     </Router>
   )
